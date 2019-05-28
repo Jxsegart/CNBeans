ca69720c1d44f2a4c42054a92dd540862cef1e8c
Jxsegart <50941446+Jxsegart@users.noreply.github.com>
2019-05-28 12:45:54 +0200

    Update GitHubProyect.java

--- GitHub Proyect/src/github/proyect/GitHubProyect.java
+++ GitHub Proyect/src/github/proyect/GitHubProyect.java
@@ -15,7 +15,7 @@ public class GitHubProyect {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    System.out.println("Loco, dese GitHub.")
+    System.out.println("Loco, dese GitHub.");
     }
     
 }
